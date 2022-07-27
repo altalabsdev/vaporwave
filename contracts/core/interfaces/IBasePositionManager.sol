aa7dec55c18ea0c07728a4615bce8e8949613c0d
Ammon Werner <ammon@altafin.co>
2022-07-26 22:44:22 -0600

    Formatting

--- contracts/core/interfaces/IBasePositionManager.sol
+++ contracts/core/interfaces/IBasePositionManager.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// Interface for BasePositionManager
 interface IBasePositionManager {
     function maxGlobalLongSizes(address _token) external view returns (uint256);
-    function maxGlobalShortSizes(address _token) external view returns (uint256);
+
+    function maxGlobalShortSizes(address _token)
+        external
+        view
+        returns (uint256);
 }
