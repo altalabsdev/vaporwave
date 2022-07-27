b8fb42794bd59589712e2e8c937c88eb09227630
Ammon Werner <ammon@altafin.co>
2022-07-26 22:47:36 -0600

    Add NatSpec

--- contracts/oracle/FastPriceEvents.sol
+++ contracts/oracle/FastPriceEvents.sol
@@ -1,23 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../libraries/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "./interfaces/IFastPriceEvents.sol";
 import "../access/Governable.sol";
 
-
+/// @title Vaporwave Fast Price Events Oracle
 contract FastPriceEvents is IFastPriceEvents, Governable {
-
-    mapping (address => bool) public isPriceFeed;
+    mapping(address => bool) public isPriceFeed;
     event PriceUpdate(address token, uint256 price, address priceFeed);
 
-    function setIsPriceFeed(address _priceFeed, bool _isPriceFeed) external onlyGov {
-      isPriceFeed[_priceFeed] = _isPriceFeed;
+    /// @notice Mark an address as a price feed
+    /// @param _priceFeed Address of the price feed to mark
+    /// @param _isPriceFeed True if the address is a price feed, false otherwise
+    function setIsPriceFeed(address _priceFeed, bool _isPriceFeed)
+        external
+        onlyGov
+    {
+        isPriceFeed[_priceFeed] = _isPriceFeed;
     }
 
+    /// @notice Emit a price event
+    /// @dev Can only be called by a price feed
+    /// @param _token Address of the token
+    /// @param _price Price of the token
     function emitPriceEvent(address _token, uint256 _price) external override {
-      require(isPriceFeed[msg.sender], "FastPriceEvents: invalid sender");
-      emit PriceUpdate(_token, _price, msg.sender);
+        require(isPriceFeed[msg.sender], "FastPriceEvents: invalid sender");
+        emit PriceUpdate(_token, _price, msg.sender);
     }
 }
