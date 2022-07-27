033edf90183b18b4066562a7da2c950a9241548b
Ammon Werner <ammon@altafin.co>
2022-07-26 22:48:53 -0600

    Upgrade to hardhat-toolbox

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,29 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-docgen";
+
 import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.5.16", // VWAVE.sol
+        version: "0.5.16", // VWAVE.sol | AuroraFactory.sol | Multicall.sol
       },
       {
-        version: "0.8.9",
+        version: "0.6.6", // AuroraRouter.sol | AuroraChef.sol
+      },
+      {
+        version: "0.8.15",
         settings: {
           viaIR: true,
-          optimizer: { enabled: true },
+          optimizer: {
+            enabled: true,
+            runs: 1,
+          },
         },
       },
     ],
@@ -51,8 +42,14 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         url: process.env.AURORA_URL || "",
+        blockNumber: 70711000,
       },
     },
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
@@ -60,8 +57,8 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      fantom: process.env.AURORASCAN_API_KEY!,
-      fantomTestnet: process.env.AURORASCAN_API_KEY!,
+      aurora: process.env.AURORASCAN_API_KEY!,
+      auroraTestnet: process.env.AURORASCAN_API_KEY!,
     },
   },
 };
