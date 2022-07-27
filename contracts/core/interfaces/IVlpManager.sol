1b6522b20b237722028bd363a86227f82383dfe5
Ammon Werner <ammon@altafin.co>
2022-07-26 22:44:14 -0600

    Formatting

--- contracts/core/interfaces/IVlpManager.sol
+++ contracts/core/interfaces/IVlpManager.sol
@@ -1,11 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// Interface for the VlpManager
 interface IVlpManager {
     function cooldownDuration() external returns (uint256);
+
     function lastAddedAt(address _account) external returns (uint256);
-    function addLiquidity(address _token, uint256 _amount, uint256 _minUsdg, uint256 _minVlp) external returns (uint256);
-    function addLiquidityForAccount(address _fundingAccount, address _account, address _token, uint256 _amount, uint256 _minUsdg, uint256 _minVlp) external returns (uint256);
-    function removeLiquidity(address _tokenOut, uint256 _vlpAmount, uint256 _minOut, address _receiver) external returns (uint256);
-    function removeLiquidityForAccount(address _account, address _tokenOut, uint256 _vlpAmount, uint256 _minOut, address _receiver) external returns (uint256);
+
+    function addLiquidity(
+        address _token,
+        uint256 _amount,
+        uint256 _minUsdv,
+        uint256 _minVlp
+    ) external returns (uint256);
+
+    function addLiquidityForAccount(
+        address _fundingAccount,
+        address _account,
+        address _token,
+        uint256 _amount,
+        uint256 _minUsdv,
+        uint256 _minVlp
+    ) external returns (uint256);
+
+    function removeLiquidity(
+        address _tokenOut,
+        uint256 _vlpAmount,
+        uint256 _minOut,
+        address _receiver
+    ) external returns (uint256);
+
+    function removeLiquidityForAccount(
+        address _account,
+        address _tokenOut,
+        uint256 _vlpAmount,
+        uint256 _minOut,
+        address _receiver
+    ) external returns (uint256);
 }
