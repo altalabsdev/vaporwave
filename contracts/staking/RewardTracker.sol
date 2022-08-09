f9b765756c98bb6eec99cf6d01e2738b653cfa31
Ammon Werner <ammon@altafin.co>
2022-08-08 23:13:14 -0600

    Complete NatSpec for RewardTracker.sol

--- contracts/staking/RewardTracker.sol
+++ contracts/staking/RewardTracker.sol
@@ -28,44 +28,65 @@ error ActionNotEnabled();
 /// Contract is already initialized
 error AlreadyInitialized();
 
-// TODO add NatSpec
 /// @title Vaporwave Reward Tracker
 contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
     using SafeERC20 for IERC20;
 
-    uint256 public constant BASIS_POINTS_DIVISOR = 10000;
-    uint256 public constant PRECISION = 1e30;
+    /// Helper to avoid truncations errors in basis points calculations
+    uint16 public constant BASIS_POINTS_DIVISOR = 10000;
+    /// Helper to avoid truncation errors in calculations
+    uint128 public constant PRECISION = 1e30;
 
-    uint8 public constant decimals = 18;
-
+    /// True if the contract is initialized
     bool public isInitialized;
 
-    string public name;
-    string public symbol;
-
+    /// The distributor address
     address public distributor;
+    /// Mapping of deposit tokens
     mapping(address => bool) public isDepositToken;
+    /// Mapping of user deposit balances
     mapping(address => mapping(address => uint256))
         public
         override depositBalances;
+    /// Mapping of total deposit supplies
     mapping(address => uint256) public totalDepositSupply;
 
+    /// The token nome
+    string public name;
+    /// The token symbol
+    string public symbol;
+    /// The token total supply
     uint256 public override totalSupply;
+    /// Mapping of user token balances
     mapping(address => uint256) public balances;
+    /// Mapping of user approved allowances
     mapping(address => mapping(address => uint256)) public allowances;
 
+    /// The cumulative reward per token
     uint256 public cumulativeRewardPerToken;
+    /// Mapping of user staked amounts
     mapping(address => uint256) public override stakedAmounts;
+    /// Mapping of user claimable rewards
     mapping(address => uint256) public claimableReward;
+    /// Mapping of previous cumulated rewards per token
     mapping(address => uint256) public previousCumulatedRewardPerToken;
+    /// Mapping of cumulative rewards
     mapping(address => uint256) public override cumulativeRewards;
+    /// Mapping of average staked amounts
     mapping(address => uint256) public override averageStakedAmounts;
 
+    /// True if contract is in private transfer mode
     bool public inPrivateTransferMode;
+    /// True if contract is in private staking mode
     bool public inPrivateStakingMode;
+    /// True if contract is in private claiming mode
     bool public inPrivateClaimingMode;
+    /// Mapping of valid handlers
     mapping(address => bool) public isHandler;
 
+    /// @notice Emitted when rewards are claimed
+    /// @param receiver The address of the receiver
+    /// @param amount The amount of tokens claimed
     event Claim(address receiver, uint256 amount);
 
     constructor(string memory _name, string memory _symbol) {
@@ -73,6 +94,9 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         symbol = _symbol;
     }
 
+    /// @notice Initializes the contract
+    /// @param _depositTokens An array of deposit token addresses
+    /// @param _distributor The distributor address
     function initialize(address[] memory _depositTokens, address _distributor)
         external
         onlyOwner
@@ -90,6 +114,9 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         distributor = _distributor;
     }
 
+    /// @notice Set `_depositToken` as a deposit token: `_isDepositToken`
+    /// @param _depositToken The deposit token address
+    /// @param _isDepositToken True to add the address as a deposit token, false to remove it
     function setDepositToken(address _depositToken, bool _isDepositToken)
         external
         onlyOwner
@@ -97,6 +124,8 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         isDepositToken[_depositToken] = _isDepositToken;
     }
 
+    /// @notice Set the contract to private transfer mode: `_inPrivateTransferMode`
+    /// @param _inPrivateTransferMode True to enable private transfer mode, false to disable it
     function setInPrivateTransferMode(bool _inPrivateTransferMode)
         external
         onlyOwner
@@ -104,6 +133,8 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         inPrivateTransferMode = _inPrivateTransferMode;
     }
 
+    /// @notice Set the contract to private staking mode: `_inPrivateStakingMode`
+    /// @param _inPrivateStakingMode True to enable private staking mode, false to disable it
     function setInPrivateStakingMode(bool _inPrivateStakingMode)
         external
         onlyOwner
@@ -111,6 +142,8 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         inPrivateStakingMode = _inPrivateStakingMode;
     }
 
+    /// @notice Set the contract to private claiming mode: `_inPrivateClaimingMode`
+    /// @param _inPrivateClaimingMode True to enable private claiming mode, false to disable it
     function setInPrivateClaimingMode(bool _inPrivateClaimingMode)
         external
         onlyOwner
@@ -118,6 +151,9 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         inPrivateClaimingMode = _inPrivateClaimingMode;
     }
 
+    /// @notice Set `_handler` as a handler: `_isActive`
+    /// @param _handler The handler address
+    /// @param _isActive True to add the address as a handler, false to remove it
     function setHandler(address _handler, bool _isActive) external onlyOwner {
         isHandler[_handler] = _isActive;
     }
@@ -135,15 +171,9 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         IERC20(_token).safeTransfer(_account, _amount);
     }
 
-    function balanceOf(address _account)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        return balances[_account];
-    }
-
+    /// @notice Stake `_amount` of `_token` tokens
+    /// @param _depositToken The address of the token to stake
+    /// @param _amount The amount to stake
     function stake(address _depositToken, uint256 _amount)
         external
         override
@@ -155,6 +185,11 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         _stake(msg.sender, msg.sender, _depositToken, _amount);
     }
 
+    /// @notice Stake `_amount` of `_token` tokens for `_account`
+    /// @param _fundingAccount The account that is funding the stake
+    /// @param _account The address of the account to stake
+    /// @param _depositToken The address of the token to stake
+    /// @param _amount The amount to stake
     function stakeForAccount(
         address _fundingAccount,
         address _account,
@@ -165,6 +200,9 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         _stake(_fundingAccount, _account, _depositToken, _amount);
     }
 
+    /// @notice Unstake `_amount` of `_token` tokens
+    /// @param _depositToken The address of the token to unstake
+    /// @param _amount The amount to unstake
     function unstake(address _depositToken, uint256 _amount)
         external
         override
@@ -176,6 +214,11 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         _unstake(msg.sender, _depositToken, _amount, msg.sender);
     }
 
+    /// @notice Unstake `_amount` of `_token` tokens for `_account`
+    /// @param _account The address of the account to unstake
+    /// @param _depositToken The address of the token to unstake
+    /// @param _amount The amount to unstake
+    /// @param _receiver The address of the account to receive the tokens
     function unstakeForAccount(
         address _account,
         address _depositToken,
@@ -186,6 +229,10 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         _unstake(_account, _depositToken, _amount, _receiver);
     }
 
+    /// @notice Transfer `_amount` tokens to `_receiver`
+    /// @param _recipient The address to receive the tokens
+    /// @param _amount The amount of tokens to transfer
+    /// @return Whether the transfer was successful
     function transfer(address _recipient, uint256 _amount)
         external
         override
@@ -195,24 +242,11 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         return true;
     }
 
-    function allowance(address _owner, address _spender)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        return allowances[_owner][_spender];
-    }
-
-    function approve(address _spender, uint256 _amount)
-        external
-        override
-        returns (bool)
-    {
-        _approve(msg.sender, _spender, _amount);
-        return true;
-    }
-
+    /// @notice Transfer `_amount` tokens from `_sender` to `_receiver`
+    /// @param _sender The address of the sender
+    /// @param _recipient The address to receive the tokens
+    /// @param _amount The amount of tokens to transfer
+    /// @return Whether the transfer was successful
     function transferFrom(
         address _sender,
         address _recipient,
@@ -234,14 +268,27 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         return true;
     }
 
-    function tokensPerInterval() external view override returns (uint256) {
-        return IRewardDistributor(distributor).tokensPerInterval();
+    /// @notice Approve `_spender` to transfer `_amount` tokens
+    /// @param _spender The address of the spender
+    /// @param _amount The amount of tokens to approve
+    /// @return Whether the approval was successful
+    function approve(address _spender, uint256 _amount)
+        external
+        override
+        returns (bool)
+    {
+        _approve(msg.sender, _spender, _amount);
+        return true;
     }
 
+    /// @notice Update rewards
     function updateRewards() external override nonReentrant {
         _updateRewards(address(0));
     }
 
+    /// @notice Claim rewards
+    /// @param _receiver The address of the account to receive the tokens
+    /// @return The amount of tokens claimed
     function claim(address _receiver)
         external
         override
@@ -254,6 +301,10 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         return _claim(msg.sender, _receiver);
     }
 
+    /// @notice Claim rewards for `_account`
+    /// @param _account The address of the account to claim tokens for
+    /// @param _receiver The address of the account to receive the tokens
+    /// @return The amount of tokens claimed
     function claimForAccount(address _account, address _receiver)
         external
         override
@@ -264,6 +315,46 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         return _claim(_account, _receiver);
     }
 
+    /// @notice Get the token balance of `_account`
+    /// @param _account The address of the account to get the balance of
+    /// @return The token balance of `_account`
+    function balanceOf(address _account)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return balances[_account];
+    }
+
+    /// @notice Get the allowance of `_spender` for `_owner`
+    /// @param _owner The address of the token owner
+    /// @param _spender The address of the spender
+    /// @return The allowance of `_spender` for `_owner`
+    function allowance(address _owner, address _spender)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return allowances[_owner][_spender];
+    }
+
+    /// @notice Get the tokens per interval
+    /// @return The tokens per interval
+    function tokensPerInterval() external view override returns (uint256) {
+        return IRewardDistributor(distributor).tokensPerInterval();
+    }
+
+    /// @notice Get the token decimals (18)
+    /// @return The token decimals (18)
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    /// @notice Get the claimable amount of tokens for `_account`
+    /// @param _account The address to query for the claimable amount of tokens
+    /// @return The claimable amount of tokens for `_account`
     function claimable(address _account)
         public
         view
@@ -286,25 +377,10 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
                     previousCumulatedRewardPerToken[_account])) / PRECISION);
     }
 
+    /// @notice Get the reward token
+    /// @return The reward token address
     function rewardToken() public view returns (address) {
         return IRewardDistributor(distributor).rewardToken();
-    }
-
-    function _claim(address _account, address _receiver)
-        private
-        returns (uint256)
-    {
-        _updateRewards(_account);
-
-        uint256 tokenAmount = claimableReward[_account];
-        claimableReward[_account] = 0;
-
-        if (tokenAmount > 0) {
-            IERC20(rewardToken()).safeTransfer(_receiver, tokenAmount);
-            emit Claim(_account, tokenAmount);
-        }
-
-        return tokenAmount;
     }
 
     function _mint(address _account, uint256 _amount) internal {
@@ -334,6 +410,23 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         totalSupply -= _amount;
 
         emit Transfer(_account, address(0), _amount);
+    }
+
+    function _claim(address _account, address _receiver)
+        private
+        returns (uint256)
+    {
+        _updateRewards(_account);
+
+        uint256 tokenAmount = claimableReward[_account];
+        claimableReward[_account] = 0;
+
+        if (tokenAmount > 0) {
+            IERC20(rewardToken()).safeTransfer(_receiver, tokenAmount);
+            emit Claim(_account, tokenAmount);
+        }
+
+        return tokenAmount;
     }
 
     function _transfer(
@@ -374,12 +467,6 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
         allowances[_owner][_spender] = _amount;
 
         emit Approval(_owner, _spender, _amount);
-    }
-
-    function _validateHandler() private view {
-        if (!isHandler[msg.sender]) {
-            revert InvalidHandler();
-        }
     }
 
     function _stake(
@@ -486,6 +573,12 @@ contract RewardTracker is IERC20, ReentrancyGuard, IRewardTracker, Ownable {
 
                 cumulativeRewards[_account] = nextCumulativeReward;
             }
+        }
+    }
+
+    function _validateHandler() private view {
+        if (!isHandler[msg.sender]) {
+            revert InvalidHandler();
         }
     }
 }
